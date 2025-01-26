42ab16741777eefa0c75c3e80bcd2337c90a3337
sakethpathike <sakethpathike@gmail.com>
2025-01-26 15:19:35 +0530

    add custom `Json` configuration to `json` for `ContentNegotiation`

--- src/main/kotlin/com/sakethh/linkora/Serialization.kt
+++ src/main/kotlin/com/sakethh/linkora/Serialization.kt
@@ -3,9 +3,14 @@ package com.sakethh.linkora
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            prettyPrint = true
+            encodeDefaults = true
+            ignoreUnknownKeys = true
+        })
     }
 }
