1b45e6ec945559db0bce16b6424e04901bcc9d91
sakethpathike <sakethpathike@gmail.com>
2025-03-07 21:49:08 +0530

    refactor: update `MoveItemsDTO` type alias

--- src/main/kotlin/com/sakethh/linkora/domain/TypeAliases.kt
+++ src/main/kotlin/com/sakethh/linkora/domain/TypeAliases.kt
@@ -1,11 +1,12 @@
 package com.sakethh.linkora.domain
 
 import com.sakethh.linkora.domain.dto.ArchiveMultipleItemsDTO
+import com.sakethh.linkora.domain.dto.MoveItemsDTO
 
 typealias PlaceHolder = String
 
 typealias PlaceHolderValue = String
 
-typealias CopyLinksDTO = MoveLinksDTO
+typealias CopyItemsDTO = MoveItemsDTO
 
 typealias DeleteMultipleItemsDTO = ArchiveMultipleItemsDTO
