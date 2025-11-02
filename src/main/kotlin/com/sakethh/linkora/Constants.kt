2b238780454a7e4ebb6094ce6bdae8049e81862d
sakethpathike <sakethpathike@gmail.com>
2025-11-02 17:54:49 +0530

    chore: bump server version to 0.2.0

--- src/main/kotlin/com/sakethh/linkora/Constants.kt
+++ src/main/kotlin/com/sakethh/linkora/Constants.kt
@@ -1,6 +1,6 @@
 package com.sakethh.linkora
 
 object Constants {
-    const val SERVER_VERSION = "0.1.0"
+    const val SERVER_VERSION = "0.2.0"
     const val KEY_STORE_ALIAS = "linkora-sync-server-cert"
 }
